2dffe0c77dacae1f0d88c4c27a80a3d2aae96ca2
abhi_test <abhi_test@gmail.com>
2021-03-26 15:59:52 +0000

    used try/except methods to check for failures

--- helpers.py
+++ helpers.py
@@ -31,8 +31,11 @@ class robot:
         if new_y <= y_size and new_y >= 0:
             self.y = new_y
 
-def actions(commands, new_robot):
+def actions(commands, old_robot):
     
+    failure = False
+    report = False
+
     facing_direction = {
     "NORTH" : 1,
     "EAST" : 2,
@@ -40,30 +43,35 @@ def actions(commands, new_robot):
     "WEST" : 4
     }
 
-    if commands[0].upper() == "PLACE" and len(commands) > 1:    # checks if initializing command is correct
-        commands[1][0] = int(commands[1][0])
-        commands[1][1] = int(commands[1][1])
-        
-        if commands[1][0] >= 0 and commands[1][0] <= x_size and commands[1][1] >= 0 and commands[1][1] <= y_size \
-        and commands[1][2].upper() in list(facing_direction.keys()):
-            new_robot = robot(commands[1][0],commands[1][1],facing_direction[commands[1][2].upper()])   # initializes robot
+    if commands[0].upper() == "PLACE" and len(commands) == 2:    # checks if initializing command is correct
+        try:
+            commands[1][0] = int(commands[1][0])
+            commands[1][1] = int(commands[1][1])
+            
+            if commands[1][0] >= 0 and commands[1][0] <= x_size and commands[1][1] >= 0 and commands[1][1] <= y_size \
+            and commands[1][2].upper() in list(facing_direction.keys()):
+                new_robot = robot(commands[1][0],commands[1][1],facing_direction[commands[1][2].upper()])   # initializes robot
 
-    elif commands[0].upper() == "MOVE":
-        new_robot.move()
+        except:
+            failure = True
 
-    elif commands[0].upper() == "RIGHT":
-        new_robot.rotate(1)
+    elif commands[0].upper() == "MOVE" and len(commands) == 1:
+        new_robot = old_robot.move()
 
-    elif commands[0].upper() == "LEFT":
-        new_robot.rotate(-1)
+    elif commands[0].upper() == "RIGHT" and len(commands) == 1:
+        new_robot = old_robot.rotate(1)
 
-    elif commands[0].upper() == "REPORT":   #sends report to stdout & a file named with the session user
-        direction = list(facing_direction.keys())[new_robot.direction - 1]
-        report = ",".join((str(new_robot.x), str(new_robot.y), direction.upper()))
-        print(report)
-        open("robot_report.txt", "a").write(report + "\n")
+    elif commands[0].upper() == "LEFT" and len(commands) == 1:
+        new_robot = old_robot.rotate(-1)
+
+    elif commands[0].upper() == "REPORT" and len(commands) == 1:   #sends report to stdout & a file named with the session user
+        direction = list(facing_direction.keys())[old_robot.direction - 1]
+        report = ",".join((str(old_robot.x), str(old_robot.y), direction.upper()))
     
-    elif commands[0] == "```":
+    elif commands[0] == "```" and len(commands) == 1:
         new_robot = False
-        
-    return new_robot
+    
+    else:
+        failure = True
+
+    return new_robot, failure, report
--- main.py
+++ main.py
@@ -4,32 +4,71 @@ from google.cloud import firestore #, storage
 
 app = Flask(__name__)
 db = firestore.Client()     # requires GCLOUD_PROJECT environment variable to be set
+sessions = db.collection('sessions')
 # storage_client = storage.Client()
 
-@app.route("/api/cli/<session_id>/<commands>")
-def cli_commands(session_id=None, commands=None):
-    command = commands.split("_")
-    doc = db.collection(u"sessions").document(session_id)   #take note of collection name
+@app.route("/api/cli/<api_uri>")
+def cli_commands(api_uri=None):
+    
+    transaction = db.transaction()
 
-    # check state in NoSQL db
-    if doc.get().exists:
-        new_robot_data = doc.get().to_dict()
+    failure = False
+
+    try:
+        command = api_uri.split("%20")
+        session_id = command[0]
+        command = command[1:]
+        try:
+            command[1] = command[1].split("%2C")
+        except: # no second argument, not needed for some actions
+            pass
+        
+    except: # splitting failed
+        failure = True
+
+    if not failure:
+        doc_ref = sessions.document(document_id=session_id)
+        doc = doc_ref.get(transaction=transaction)   #take note of collection name
+        if doc.exists:  # get data from NoSQL db (firestore)
+            new_robot_data = doc.get().to_dict()
+
+        else:   # new session giving command
+            new_robot_data = {
+                "x" : None,
+                "y" : None,
+                "direction": None
+            }
+
+            transaction.set(doc_ref,new_robot_data)    # init session document in firestore
+            
         new_robot = robot(new_robot_data["x"], new_robot_data["y"], new_robot_data["direction"])
+
     else:
         new_robot = False
     
-    if new_robot:
-        new_robot = actions(command, new_robot)
+    new_robot, failure, report = actions(command, new_robot)    #attempts to execute commands, failure returns false if valid command
 
-        #set updated state into NoSQL db
-        doc.set({
-            "x" : new_robot.x,
-            "y" : new_robot.y,
-            "direction" : new_robot.direction
-        })
-        return jsonify({"message": "Command sent!"})
-    else:
+    if failure:
         return jsonify({"message": "Invalid command"})
+        
+    else:
+
+        if report:
+            return jsonify({"message": report})
+        
+        else:
+
+            new_robot_data = {
+                "x" : new_robot.x,
+                "y" : new_robot.y,
+                "direction" : new_robot.direction
+            }
+
+            transaction.set(doc_ref, new_robot_data)    #set updated state into NoSQL db
+
+            return jsonify({"message": "Command sent!"})
+
+    
 
 # @app.route("/api/file_upload/<session_id>")
 # def file_upload_commands(session_id=None):
