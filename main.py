8b063e51736e3d64bcdead382cae3df35029b753
abhi_test <abhi_test@gmail.com>
2021-03-27 13:41:44 +0000

    change files for file_upload api setup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from helpers import robot, actions, respond
 from flask import Flask, jsonify, request
-from google.cloud import firestore #, storage
+from google.cloud import firestore
 
 app = Flask(__name__)
 db = firestore.Client()     # requires GCLOUD_PROJECT environment variable to be set
@@ -39,7 +39,7 @@ def set_session_data(transaction, session_id, new_robot_data):
     doc_ref = sessions.document(document_id=session_id)
     transaction.set(doc_ref, new_robot_data)
 
-@app.route("/api/cli/<api_uri>", methods=["GET"])
+@app.route("/api/cli/<api_uri>", methods=["POST"])
 def cli_commands(api_uri):
     
     transaction = db.transaction()
@@ -80,38 +80,6 @@ def cli_commands(api_uri):
             message = {"message": "Command sent!"}
 
     return respond(message)
-
-    
-
-# @app.route("/api/file_upload/<session_id>")
-# def file_upload_commands(session_id=None):
-#     bucket = storage_client.bucket("toy_robot_uploaded_instructions")     #take note of bucket name
-#     bucket.blob(session_id).download_to_filename("import_instructions.txt")
-    
-#     # check state in NoSQL db
-#     doc = db.collection(u"sessions").document(session_id)
-#     if doc.get().exists:
-#         new_robot_data = doc.get().to_dict()
-#         new_robot = robot(new_robot_data["x"], new_robot_data["y"], new_robot_data["direction"])
-#     else:
-#         new_robot = False
-    
-#     # Execute instructions from file
-#     lines = open("import_instructions.txt", "r").readlines()
-#     for line in lines:
-#         line = line.replace("\n","")
-#         command = line.split(" ")
-#         if len(command) > 1:
-#             command[1] = command[1].split(",")
-
-#         new_robot = actions(command, new_robot)
-    
-#     #set updated state into NoSQL db
-#     doc.set({
-#         "x" : new_robot.x,
-#         "y" : new_robot.y,
-#         "direction" : new_robot.direction
-#     })
 
 if __name__ == "__main__":
     app.run(port=8080, debug=True)
