781074a835eeeaa45a55a9cd6cb9b3109eb1ed4d
abhi_test <abhi_test@gmail.com>
2021-03-27 14:21:16 +0000

    test if json package is sent both ways

--- main.py
+++ main.py
@@ -15,71 +15,73 @@ try:
 except ImportError:
   pass
 
-@firestore.transactional
-def get_session_data(transaction, session_id):
-    doc_ref = sessions.document(document_id=session_id)
-    doc = doc_ref.get(transaction=transaction)
-    if doc.exists:
-        new_robot_data = doc.to_dict()
-    else:
-        new_robot_data = {
-                "x" : None,
-                "y" : None,
-                "direction": None
-            }
+# @firestore.transactional
+# def get_session_data(transaction, session_id):
+#     doc_ref = sessions.document(document_id=session_id)
+#     doc = doc_ref.get(transaction=transaction)
+#     if doc.exists:
+#         new_robot_data = doc.to_dict()
+#     else:
+#         new_robot_data = {
+#                 "x" : None,
+#                 "y" : None,
+#                 "direction": None
+#             }
 
-        transaction.set(doc_ref, new_robot_data)
+#         transaction.set(doc_ref, new_robot_data)
 
-    new_robot = robot(new_robot_data["x"], new_robot_data["y"], new_robot_data["direction"])
+#     new_robot = robot(new_robot_data["x"], new_robot_data["y"], new_robot_data["direction"])
 
-    return new_robot
+#     return new_robot
 
-@firestore.transactional
-def set_session_data(transaction, session_id, new_robot_data):
-    doc_ref = sessions.document(document_id=session_id)
-    transaction.set(doc_ref, new_robot_data)
+# @firestore.transactional
+# def set_session_data(transaction, session_id, new_robot_data):
+#     doc_ref = sessions.document(document_id=session_id)
+#     transaction.set(doc_ref, new_robot_data)
 
-@app.route("/api/cli/<api_uri>", methods=["POST"])
-def cli_commands(api_uri):
+@app.route("/api/file_upload/", methods=["POST"])
+def process_request():
     
-    transaction = db.transaction()
+    # transaction = db.transaction()
 
-    failure = False
+    # failure = False
 
+    commands = request.json
+    return respond(commands)
 
-    command = api_uri.split(" ")
-    session_id = command[0]
-    command = command[1:]
-    try:
-        command[1] = command[1].split(",")
-    except: # no second argument, not needed for some actions
-        pass
+    # command = api_uri.split(" ")
+    # session_id = command[0]
+    # command = command[1:]
+    # try:
+    #     command[1] = command[1].split(",")
+    # except: # no second argument, not needed for some actions
+    #     pass
 
-    new_robot = get_session_data(transaction, session_id)
+    # new_robot = get_session_data(transaction, session_id)
     
-    new_robot, failure, report = actions(command, new_robot)    #attempts to execute commands, failure returns false if valid command
+    # new_robot, failure, report = actions(command, new_robot)    #attempts to execute commands, failure returns false if valid command
 
-    if failure:
-        message = {"message": "Invalid Command!"}
+    # if failure:
+    #     message = {"message": "Invalid Command!"}
         
-    else:
+    # else:
 
-        if report:
-            message = {"message": report}
+    #     if report:
+    #         message = {"message": report}
         
-        else:
+    #     else:
 
-            new_robot_data = {
-                "x" : new_robot.x,
-                "y" : new_robot.y,
-                "direction" : new_robot.direction
-            }
+    #         new_robot_data = {
+    #             "x" : new_robot.x,
+    #             "y" : new_robot.y,
+    #             "direction" : new_robot.direction
+    #         }
 
-            set_session_data(transaction, session_id, new_robot_data)    #set updated state into NoSQL db
+    #         set_session_data(transaction, session_id, new_robot_data)    #set updated state into NoSQL db
 
-            message = {"message": "Command sent!"}
+    #         message = {"message": "Command sent!"}
 
-    return respond(message)
+    # return respond(message)
 
 if __name__ == "__main__":
     app.run(port=8080, debug=True)
