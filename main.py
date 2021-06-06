6518666dae8471a501708f0fe828b30bef39c91c
abhi_test <abhi_test@gmail.com>
2021-06-06 13:13:35 +0000

    changed to server side api call, index.html fetch uri calls same server -> server calls cli-svc over kube dns

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from uuid import uuid4
 
 from flask import Flask, make_response, request, jsonify, request, render_template
 from google.cloud import firestore
-import os
+import os, requests
 
 api_key = os.environ.get("API_KEY")
 app = Flask(__name__)
@@ -39,7 +39,6 @@ def get_session_data(transaction, session_id):
 @app.route('/', methods=['GET'])
 def home():
     
-
     transaction = db.transaction()
     session = get_session_data(transaction, request.cookies.get('session_id'))
 
@@ -47,6 +46,14 @@ def home():
     resp = make_response(template)
     resp.set_cookie('session_id', session['session_id'], httponly=True)
     return resp
+
+@app.route(f"/api/cli/{api_key}/<api_uri>", methods=["GET"])
+def cli_commands(api_uri):
+
+    url = f"http://toy-robot-cli-svc.production.svc.cluster.local/api/cli/{api_key}/{api_uri}"
+    cli_response = requests.get(url)
+
+    return cli_response
 
 
 if __name__ == '__main__':
